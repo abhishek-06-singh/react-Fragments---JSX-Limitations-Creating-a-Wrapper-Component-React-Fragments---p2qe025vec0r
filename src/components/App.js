4ce3bc766affc89a9ccc3a05fc2f0c1fe5054c57
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2023-01-07 21:14:49 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,18 @@
 import React, { useState, useEffect, Fragment } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 import List from "./List";
 
 const App = () => {
- 
   const [value, setValue] = useState(0);
   const [list, setList] = useState([]);
 
-
   const onButtonClick = () => {
-    setList([])
-    for (let i = 1; i < Number(value)+1; i++) {
-      
+    setList([]);
+    for (let i = 1; i < Number(value) + 1; i++) {
       setList((prev) => [...prev, i]);
     }
-   
   };
- 
 
   const onInputChange = (e) => {
     setValue(e.target.value);
@@ -25,11 +20,12 @@ const App = () => {
 
   return (
     <div id="main">
-
-      <input id="input" onChange={onInputChange} />
-      <button id="button" onClick={onButtonClick}>Click</button>
-       <ul id="list">
-      <List listx={list} />
+      <input id="input" type="number" onChange={onInputChange} />
+      <button id="button" onClick={onButtonClick}>
+        Click
+      </button>
+      <ul id="list">
+        <List listx={list} />
       </ul>
     </div>
   );
--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ListItems from './ListItems';
+import React from "react";
+import ListItems from "./ListItems";
 
- const List = ({listx}) =>{
-  return(
-    // code here
-    
-      <ListItems  />
-    
-    
-  )
-}
+const List = ({ listx }) => {
+  return (
+    // code here [5,4,6,7]
+    <React.Fragment>
+      {listx.map((e) => {
+        return <ListItems valuex={e} />;
+      })}
+    </React.Fragment>
+  );
+};
 
 export default List;
